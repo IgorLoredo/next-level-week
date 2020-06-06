360d446d9cc14384745f928a96c2397e85d6a34d
IgorLoredo <igorloredo12@gmail.com>
2020-06-06 00:09:57 -0300

    função selete do cadrastro funcionando

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -28,17 +28,19 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = false;
 
     fetch(url)
     .then(res => res.json() )
     .then( cities => {
-
+        
         for(const city of cities ){
-            citySelect.innerHTML += ` <option value ="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += ` <option value ="${city.nome}">${city.nome}</option>`
         }       
     } )
 
-    citySelect.disabled = false;
+    
 
 }
 
@@ -47,3 +49,19 @@ document
     .querySelector("select[name =uf]")
     .addEventListener("change", getCities)
 
+// itens de coleta
+//pegar todo so li
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+    for(const item of itemsToCollect){
+        item.addEventListener("click",handleSelectedItem)
+    }
+
+    function handleSelectedItem(event){
+        const itemLi = event.target
+
+        itemLi.classList.toggle("selected")
+
+        const itemId = itemLi.dataset.id
+        
+}
